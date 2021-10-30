ac9c9d0d6d70b8fe73f7886e4007b9ead96a2997
Nikolai Makarevich <nikoladze529@mail.ru>
2021-10-30 10:20:47 +0300

    feat: solve 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+const fsp = require('fs/promises');
+const fs = require('fs')
+const path = require('path');
+
+const source = path.join(__dirname, 'styles');
+const dist = path.join(__dirname, 'project-dist', 'bundle.css');
+let writeStream = fs.createWriteStream(dist);
+
+fsp.readdir(source, {withFileTypes: true})
+  .then(array => array.filter(item => item.isFile() && path.extname(path.join(source, item.name)) === '.css'))
+  .then(files => files.forEach(file => {
+    let readStream = fs.createReadStream(path.join(source, file.name), 'utf-8');
+    readStream.pipe(writeStream)
+  }))
+
+  // let readStream = fs.createReadStream(path.join(source, file.name), 'utf-8');
+  //   let writeStream = fs.createWriteStream(dist);
+  //   readStream.pipe(writeStream)
